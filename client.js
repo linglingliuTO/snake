9069faed4953659878cf00e9777dd1e0840b91ba
linglingliuTO <linglingliu.to@gmail.com>
2022-08-13 14:52:45 +0000

    add readme

--- client.js
+++ client.js
@@ -4,8 +4,8 @@ const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   
   
